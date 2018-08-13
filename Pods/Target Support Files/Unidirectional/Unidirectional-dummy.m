fe256535fc080fa53a9f5ab7c29592859e103bee
pteasima <ptea.sima@gmail.com>
2018-08-13 23:45:32 +0200

    modified gitignore

--- Pods/Target Support Files/Unidirectional/Unidirectional-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Unidirectional : NSObject
-@end
-@implementation PodsDummy_Unidirectional
-@end
--- Pods/Unidirectional/Unidirectional/Classes/Common.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-public typealias Dispatch<Action> = (Action) -> ()
-public typealias Reduce<State, Effect: Effect> = (inout State, Effect.Action) -> Effect
