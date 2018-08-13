29d6387f35d416a9bed6bd7dfa84248a8cba69e5
pteasima <ptea.sima@gmail.com>
2018-08-14 00:31:10 +0200

    added test target

---
+++ SmartContractorFrameworkTests/HelloTest.swift
@@ -0,0 +1,24 @@
+//
+//  HelloTest.swift
+//  SmartContractorFrameworkTests
+//
+//  Created by Petr Šíma on 14/08/2018.
+//
+
+import XCTest
+
+class HelloTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+      
+    }
+
+}
